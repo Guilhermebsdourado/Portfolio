d99481f8de092aad06cc03b947b636b07cc7cbc0
Guilhermebsdourado <guilhermebarsilva@gmail.com>
2025-08-29 23:13:17 -0300

    foto redonda e scroll

---
+++ src/js/scroll.js
@@ -0,0 +1,15 @@
+window.revelar = ScrollReveal({reset: true})
+
+revelar.reveal('.scroll', {
+    duration: 2500,
+    distance: '120px'
+});
+
+revelar.reveal('.scroll2', {
+    duration: 2500,
+    rotate: {x: 0, y: 90, z: 0}
+});
+revelar.reveal('.scroll3', {
+    duration: 2500,
+    rotate: {x: 20, y: 90, z: 0}
+});
